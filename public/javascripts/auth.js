a3ec7d08e61c49e43c17508d5bf73b271fef90d2
ShaneOGrady97 <36577751+ShaneOGrady97@users.noreply.github.com>
2018-03-18 23:33:44 +0000

    Update auth.js

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -54,6 +54,62 @@ $(document).ready(
     Cookies.remove('Authorization');            
     $(location).attr('href', '/');
 });
+    $("#register_error_message").hide();
+    $("#password_error_message").hide();
+    $("#retype_password_error_message").hide();
 
+    var error_email = false;
+    var error_password = false;
+    var error_retype_password = false;
+    
+    $( "#inputUsername" ).focusout(function() {
+        check_email();
+    });
+
+    $( "#inputPassword" ).focusout(function() {
+        check_password();
+    });
+
+    $( "#retypePassword" ).focusout(function() {
+        check_retype_password();
+    });
+
+     function check_email() {
+        var pattern = new RegExp(/^\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i);
+
+        if(pattern.test($("#inputUsername").val())) {
+            $("#register_error_message").hide();
+        } else {
+            $("#register_error_message").html("Invalid email address");
+            $("#register_error_message").show();
+            error_email=true;
+        }
+    }
+
+     function check_password() {
+        var password_length = $("#inputPassword").val().length;
+
+        if(password_length < 5) {
+            $("#password_error_message").html("At least 5 characters");
+            $("#password_error_message").show();
+            error_password=true;
+        } else {
+            $("#password_error_message").hide();
+        }
+    }
+
+    function check_retype_password() {
+        var password = $("#inputPassword").val();
+        var retype_password = $("#retypePassword").val();
+
+
+        if(password != retype_password) {
+            $("#retype_password_error_message").html("Password does not match");
+            $("#retype_password_error_message").show();
+            error_retype_password=true;
+        } else {
+            $("#retype_password_error_message").hide();
+        }
+    }
 
     });
